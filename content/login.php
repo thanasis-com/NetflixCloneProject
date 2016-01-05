b2631681fe0854f3b7c83d1fead6ce67ec30fb3d
Thanasis <tsiarasa@hotmail.com>
2016-01-05 13:59:08 +0000

    Create login.php

---
+++ content/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>Login</title>
+<link href="styles/style1.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+<div id="container">
+<div id="content">
+
+<h2>Πιστοποίηση</h2><br></br>
+<form action="check.php" method="post">
+	Username: <input type="text" name="user"/>
+	<br></br>
+	Password:&nbsp; <input type="password" name="pass"/>
+	<br></br>
+	<input type="submit" name="login" value="Login"/>
+	
+</form>
+</div>
+</div>
+</body>
+
+</html>
