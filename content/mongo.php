c20c1192aa723534125b71cbdf99c230f8f7c7bb
Thanasis <tsiarasa@hotmail.com>
2016-01-10 22:16:31 +0000

    Update mongo.php

--- content/mongo.php
+++ content/mongo.php
@@ -1,6 +1,6 @@
 <?php
 try{
-$connection = 0; // connect to a remote host (default port: 27017)
+$connection =shit; // connect to a remote host (default port: 27017)
 }catch (Exception $e) {
   echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
