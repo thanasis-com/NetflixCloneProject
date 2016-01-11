03c26e23bc906b652c173be077dc5655040d5cc2
Thanasis <tsiarasa@hotmail.com>
2016-01-11 18:09:28 +0000

    Update mongo.php

--- content/mongo.php
+++ content/mongo.php
@@ -1,3 +1,14 @@
 <?php
 $connection = new Mongo("mongodb://172.17.0.4");
+echo "Connection to database successfully";
+$db = $connection->CATALOGUE;
+echo "Database mydb selected";
+$collection = $db->movies;
+echo "Collection selected succsessfully";
+$cursor = $collection->find();
+   // iterate cursor to display title of documents
+	
+   foreach ($cursor as $document) {
+      echo $document["title"] . "\n";
+   }
 ?>
