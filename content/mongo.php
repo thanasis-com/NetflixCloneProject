bb0cac7a31e2e4a93c158ac42317b839283f4006
Thanasis <tsiarasa@hotmail.com>
2016-01-11 17:55:27 +0000

    Update mongo.php

--- content/mongo.php
+++ content/mongo.php
@@ -1,7 +1,3 @@
 <?php
-try{
-$connection =new MongoClient( "mongodb://172.17.0.4" ); // connect to a remote host (default port: 27017)
-}catch (Exception $e) {
-  echo 'Caught exception: ',  $e->getMessage(), "\n";
-}
+$connection = new Mongo();
 ?>
