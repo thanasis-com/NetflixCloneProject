73cdba3658891f5ea6772731f3cde23c2eab7a0f
Thanasis <tsiarasa@hotmail.com>
2016-01-13 20:27:24 +0000

    Create fetchsomevideos.php

---
+++ content/fetchsomevideos.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+if(!isset($_SESSION['role']))
+{
+	header("location:login.php");
+	}
+
+function fetchsome($genre)
+{
+  $connection = new Mongo("mongodb://172.17.0.2");
+  $db = $connection->CATALOGUE;
+  $collection = $db->movies;
+  $cursor = $collection->find();
+  foreach ($cursor as $document) {
+    if(($document["visibility"]==$_SESSION['role'] || $_SESSION['role']=="prime" || $_SESSION['role']=="admin")&&($document["genre"]==$genre)){
+		
+      echo '<p>'.$document["title"].'</p>'.'<video class="video-js vjs-default-skin" width="640px" height="267px" controls preload="auto" poster="pics/bbb.png" data-setup="{}"> <source src="rtmp://netflixcloneproject.cloudapp.net:1935/vod2/'.$document["filename"].'" type="rtmp/mp4" /></video>'.'<p>Description: '.$document["description"].'</p>'.'<p>Genre: '.$document["genre"].'</p>';
+    }
+   }
+}
+?>
