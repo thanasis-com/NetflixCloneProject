ed6132b035c0ed5b72607c73eab54450ef4f3109
Thanasis <tsiarasa@hotmail.com>
2016-01-13 18:20:34 +0000

    Update fetchvideos.php

--- content/fetchvideos.php
+++ content/fetchvideos.php
@@ -9,7 +9,7 @@ $db = $connection->CATALOGUE;
 $collection = $db->movies;
 $cursor = $collection->find();
 foreach ($cursor as $document) {
-    if($document["role"]==$_SESSION['role'] || $_SESSION['role']==prime || $_SESSION['role']=admin){
+    if($document["role"]==$_SESSION['role'] || $_SESSION['role']=="prime" || $_SESSION['role']=="admin"){
 		
       echo '<p>'.$document["title"].'</p>'.'<video class="video-js vjs-default-skin" width="640px" height="267px" controls preload="auto" poster="pics/bbb.png" data-setup="{}"> <source src="rtmp://netflixcloneproject.cloudapp.net:1935/vod2/'.$document["filename"].'" type="rtmp/mp4" /></video>'.'<p>Description: '.$document["description"].'</p>'.'<p>Genre: '.$document["genre"].'</p>';
     }
