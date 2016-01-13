32330e301b1962174b8ed57decb946dd79f7c892
Thanasis <tsiarasa@hotmail.com>
2016-01-13 17:05:20 +0000

    Update fetchstandard.php

--- content/fetchstandard.php
+++ content/fetchstandard.php
@@ -14,7 +14,7 @@ $cursor = $collection->find();
 echo '<link href="http://vjs.zencdn.net/4.11/video-js.css" rel="stylesheet"><script src="http://vjs.zencdn.net/4.11/video.js"></script>';
 
 foreach ($cursor as $document) {
-      echo '<p>'.$document["title"].'</p>'.'<video class="video-js vjs-default-skin" width="640px" height="267px" controls preload="none" poster='pics/bbb.png' data-setup="{ "aspectRatio":"640:267", "playbackRates": [1, 1.5, 2] }"> <source src="rtmp://netflixcloneproject.cloudapp.net:1935/vod2/bbb.mp4" type="rtmp/mp4" /></video>'.'<p>Description: '.$document["description"].'</p>'.'<p>Genre: '.$document["genre"].'</p>';
+      echo '<p>'.$document["title"].'</p>'.'<video class="video-js vjs-default-skin" width="640px" height="267px" controls preload="none" poster="pics/bbb.png" data-setup="{ "aspectRatio":"640:267", "playbackRates": [1, 1.5, 2] }"> <source src="rtmp://netflixcloneproject.cloudapp.net:1935/vod2/bbb.mp4" type="rtmp/mp4" /></video>'.'<p>Description: '.$document["description"].'</p>'.'<p>Genre: '.$document["genre"].'</p>';
       
    }
 
