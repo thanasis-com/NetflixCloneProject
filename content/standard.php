5fe6e147547d03546b7850b77cb8570afc0c43fe
Thanasis <tsiarasa@hotmail.com>
2016-01-13 17:14:22 +0000

    Update standard.php

--- content/standard.php
+++ content/standard.php
@@ -22,7 +22,7 @@ if(!isset($_SESSION['role']))
 
 <body>
 <!-- #BeginEditable -->
-<?php include 'fetchstandard.php'; ?>
+
 <!-- #EndEditable -->
 
 <!-- Begin Container -->
@@ -47,7 +47,7 @@ if(!isset($_SESSION['role']))
 	<!-- Begin Content -->
 	<div id="content">
 		<!-- #BeginEditable "content" -->
-		
+		<?php include 'fetchstandard.php'; ?>
 		
 		<!-- #EndEditable "content" --></div>
 	<!-- End Content -->
