7b2787801eda732755c04b2f20c7967e95780762
Thanasis <tsiarasa@hotmail.com>
2016-01-13 18:08:04 +0000

    Update fetchstandard.php

--- content/fetchstandard.php
+++ content/fetchstandard.php
@@ -6,7 +6,7 @@ if(!isset($_SESSION['role']))
 	header("location:login.php");
 	}
 
-$connection = new Mongo("mongodb://172.17.0.1");
+$connection = new Mongo("mongodb://172.17.0.2");
 $db = $connection->CATALOGUE;
 $collection = $db->movies;
 $cursor = $collection->find();
