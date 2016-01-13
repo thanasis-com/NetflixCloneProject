7cb14a2320c72235975cc5a10cac70065f1f3cba
Thanasis <tsiarasa@hotmail.com>
2016-01-13 18:16:44 +0000

    Update fetchvideos.php

--- content/fetchvideos.php
+++ content/fetchvideos.php
@@ -9,6 +9,8 @@ $db = $connection->CATALOGUE;
 $collection = $db->movies;
 $cursor = $collection->find();
 foreach ($cursor as $document) {
+    if($document["role"]==$_SESSION['role'] || $document["role"]==prime || $document["role"]=admin)
+		
       echo '<p>'.$document["title"].'</p>'.'<video class="video-js vjs-default-skin" width="640px" height="267px" controls preload="auto" poster="pics/bbb.png" data-setup="{}"> <source src="rtmp://netflixcloneproject.cloudapp.net:1935/vod2/'.$document["filename"].'" type="rtmp/mp4" /></video>'.'<p>Description: '.$document["description"].'</p>'.'<p>Genre: '.$document["genre"].'</p>';
       
    }
