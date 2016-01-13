b3cc8cfa9888bb2299d7c1b8d82cd61f25a4217e
Thanasis <tsiarasa@hotmail.com>
2016-01-13 16:41:48 +0000

    Update fetchstandard.php

--- content/fetchstandard.php
+++ content/fetchstandard.php
@@ -12,13 +12,13 @@ $collection = $db->movies;
 $cursor = $collection->find();
 
 foreach ($cursor as $document) {
-      echo '<p>'.$document["title"].'</p>'-
+      echo '<p>'.$document["title"].'</p>'.
       '<video id="my_video_1" class="video-js vjs-default-skin" width="640px" height="267px"
 	     controls preload="none"
 	     data-setup='{ "aspectRatio":"640:267", "playbackRates": [1, 1.5, 2] }'>
 	<source src="rtmp://netflixcloneproject.cloudapp.net:1935/vod2/bbb.mp4" type='rtmp/mp4' />
 	<!--<source src="http://vjs.zencdn.net/v/oceans.webm" type='video/webm' />-->
-      </video>'-
+      </video>'.
       '<p>Description: '.$document["description"].'</p>'.'<p>Genre: '.$document["genre"].'</p>';
       
    }
