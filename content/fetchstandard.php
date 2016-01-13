ff5a2150b14f35d68f1106b1548411540cff9dec
Thanasis <tsiarasa@hotmail.com>
2016-01-13 16:32:56 +0000

    Update fetchstandard.php

--- content/fetchstandard.php
+++ content/fetchstandard.php
@@ -1,14 +1,19 @@
 <?php
 
+session_start();
+if(!isset($_SESSION['role']))
+{
+	header("location:login.php");
+	}
+
 $connection = new Mongo("mongodb://172.17.0.4");
 $db = $connection->CATALOGUE;
 $collection = $db->movies;
 $cursor = $collection->find();
 
 foreach ($cursor as $document) {
-      echo $document["title"] . "\n"."\n";
-      echo "Description: ".$document["description"]."\n";
-      echo "Genre: ".$document["genre"];
+      echo '<p>'.$document["title"].'</p>'.'<p>Description: '.$document["description"].'</p>'.'<p>Genre: '.$document["genre"].'</p>'
+      
    }
 
 ?>
