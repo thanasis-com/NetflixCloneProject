c5fe85e441a4513fd36a48dda85ae0f8dfae2fe0
Thanasis <tsiarasa@hotmail.com>
2016-01-13 17:51:23 +0000

    Update fetchstandard.php

--- content/fetchstandard.php
+++ content/fetchstandard.php
@@ -12,7 +12,7 @@ $collection = $db->movies;
 $cursor = $collection->find();
 
 foreach ($cursor as $document) {
-      echo '<p>'.$document["title"].'</p>'.'<video class="video-js vjs-default-skin" width="640px" height="267px" controls preload="auto" poster="pics/bbb.png" data-setup="{}"> <source src="rtmp://netflixcloneproject.cloudapp.net:1935/vod2/bbb.mp4" type="rtmp/mp4" /></video>'.'<p>Description: '.$document["description"].'</p>'.'<p>Genre: '.$document["genre"].'</p>';
+      echo '<p>'.$document["title"].'</p>'.'<video class="video-js vjs-default-skin" width="640px" height="267px" controls preload="auto" poster="pics/bbb.png" data-setup="{}"> <source src="rtmp://netflixcloneproject.cloudapp.net:1935/vod2/'.$document["filename"].'" type="rtmp/mp4" /></video>'.'<p>Description: '.$document["description"].'</p>'.'<p>Genre: '.$document["genre"].'</p>';
       
    }
 
