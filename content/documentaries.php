98d46f1d964bd1c3b71af1b6055f20b4de872f5b
Thanasis <tsiarasa@hotmail.com>
2016-01-13 20:15:52 +0000

    Create documentaries.php

---
+++ content/documentaries.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+if(!isset($_SESSION['role']))
+{
+	header("location:login.php");
+	}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
+
+<!-- #BeginTemplate "master.dwt" -->
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<!-- #BeginEditable "doctitle" -->
+<title>Documentaries</title>
+<!-- #EndEditable -->
+<link href="styles/style1.css" rel="stylesheet" type="text/css" />
+<link href="http://vjs.zencdn.net/4.11/video-js.css" rel="stylesheet">
+<script src="http://vjs.zencdn.net/4.11/video.js"></script>
+</head>
+
+<body>
+<!-- #BeginEditable -->
+
+<!-- #EndEditable -->
+
+<!-- Begin Container -->
+<div id="container">
+	
+	<!-- Begin Masthead -->
+	<div id="masthead">
+		<!-- #BeginEditable -->
+		<h1>Documentaries</h1>
+		<!-- #EndEditable -->
+	</div>
+	<!-- End Masthead -->
+	<!-- Begin Navigation -->
+	<div id="navigation">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="animated.php">Animated videos</a></li>
+			<li><a href="documentaries.php">Documentaries</a></li>
+			<li><a href="music.php">Music videos</a></li>
+		</ul>
+	</div>
+	<!-- End Navigation -->
+	<!-- Begin Content -->
+	<div id="content">
+		<!-- #BeginEditable "content" -->
+		<div id="welcome">
+		
+		</div>
+		
+		<!-- #EndEditable "content" --></div>
+	<!-- End Content -->
+	<!-- End Container -->
+</div>
+</body>
+
+<!-- #EndTemplate -->
+
+</html>
