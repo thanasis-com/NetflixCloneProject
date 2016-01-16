6fa691d48fe8aa5a0cdec0a4f05f51d3e3d9a754
Thanasis <tsiarasa@hotmail.com>
2016-01-16 10:51:21 +0000

    Update logininfo.php

--- content/logininfo.php
+++ content/logininfo.php
@@ -6,6 +6,6 @@ if(!isset($_SESSION['role']))
 	header("location:login.php");
 	}
 
-echo '<p class="navicontent"><div class="label">Logged in as: </div>'.$_SESSION['name'].$_SESSION['surname'].'</p><p><div class="label">User subscription: </div>'.$_SESSION['role'];
+echo '<p><div class="label">Logged in as: </div>'.$_SESSION['name'].$_SESSION['surname'].'</p><p><div class="label">User subscription: </div>'.$_SESSION['role'].'</p>';
 	
 ?>
