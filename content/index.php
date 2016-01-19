72adc24407a97406687af0c95a723eaa53d8fd7a
Thanasis <tsiarasa@hotmail.com>
2016-01-19 20:38:56 +0000

    Update index.php

--- content/index.php
+++ content/index.php
@@ -32,7 +32,7 @@ if(!isset($_SESSION['role']))
 	<!-- Begin Masthead -->
 	<div id="masthead">
 		<!-- #BeginEditable -->
-		<h1>Homess</h1>
+		<h1>Home</h1>
 		<!-- #EndEditable -->
 	</div>
 	<!-- End Masthead -->
