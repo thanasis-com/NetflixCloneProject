b53ec7740b77d21f5f3c7ae998f9ef95ae82722b
Thanasis <tsiarasa@hotmail.com>
2016-01-20 19:49:42 +0000

    Update like.php

--- content/like.php
+++ content/like.php
@@ -11,7 +11,7 @@ echo $id;
 $connection = new Mongo("mongodb://172.17.0.3");
 $db = $connection->CATALOGUE;
 $collection = $db->movies;
-
+$id=2;
 $criteria = array("_id"=>$id);
 $newdata = array('$inc'=>array("likes"=>1));
 
