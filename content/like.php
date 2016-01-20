b504d93183c8c74ddaafedf6a845f77de95c86c7
Thanasis <tsiarasa@hotmail.com>
2016-01-20 17:50:32 +0000

    Update like.php

--- content/like.php
+++ content/like.php
@@ -1,6 +1,21 @@
 <?php
-header("Location:login.php");
+session_start();
+if(!isset($_SESSION['role']))
+{
+	header("location:login.php");
+	}
+?>
 
+$connection = new Mongo("mongodb://172.17.0.3");
+$db = $connection->CATALOGUE;
+$collection = $db->movies;
 
+$criteria = array("_id"=>1);
+$newdata = array('$inc'=>array("likes"=>1));
+
+$collection->update(
+            $criteria,
+            $newdata,
+          );
 
 ?>
