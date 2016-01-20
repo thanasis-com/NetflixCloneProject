2a614f1aacdce0830dc72cd92d68d379be2262ae
Thanasis <tsiarasa@hotmail.com>
2016-01-20 19:53:35 +0000

    Update index.php

--- content/index.php
+++ content/index.php
@@ -23,7 +23,7 @@ if(!isset($_SESSION['role']))
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 <script type="text/javascript">
 function like(value) {
-    $.post('like.php', {id:value});
+    $.post("like.php", {id:value});
     location.reload();
     return false;
 }
