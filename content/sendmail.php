e58e09019319cadb467f27d68dadf326f5280d38
Thanasis <tsiarasa@hotmail.com>
2016-01-22 18:28:45 +0000

    Update sendmail.php

--- content/sendmail.php
+++ content/sendmail.php
@@ -1,5 +1,35 @@
 <?php
+session_start();
+if(!isset($_SESSION['role']))
+{
+	header("location:login.php");
+	}
 
-mail ("tsiarasa@hotmail.com","This is subject","bullshit","From:netflixclone@project.com");
+$id = intval($_POST['id']);
+
+$connection = new Mongo("mongodb://172.17.0.3");
+$db = $connection->CATALOGUE;
+$collection = $db->movies;
+$criteria = array("_id"=>$id);
+$cursor = $collection->find($criteria);
+
+$title=$cursor['title'];
+$moviegenre=$cursor['genre'];
+
+$cursor = $collection->find();
+
+$message='Hello '.$_SESSION['name'].',\n\n'.'We noticed you liked '.$title.'. \n We have more videos like this: \n';
+
+$sendflag=false;
+foreach ($cursor as $document) {
+  if($moviegenre==$document['genre']){
+  $sendflag=true;
+  $message.=$document.'\n';
+  }
+}
+
+if(sendflag){
+mail ($_SESSION['email'],"Videos you may like!",$message,"From:netflixclone@project.com");
+}
 
 ?>
