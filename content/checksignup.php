6cc3085883ddda0228ac73a41ca82fabedb4a982
Thanasis <tsiarasa@hotmail.com>
2016-01-23 16:30:07 +0000

    Update checksignup.php

--- content/checksignup.php
+++ content/checksignup.php
@@ -3,7 +3,7 @@
 $name=$_POST['name'];
 $surname=$_POST['surname'];
 $email=$_POST['email'];
-$pass=hash('sha512',$_POST['password']);
+$pass=md5($_POST['password']);
 $age=$_POST['age'];
 $country=$_POST['country'];
 $role=$_POST['role'];
@@ -18,15 +18,33 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
+/*
 $sql = "INSERT INTO users (name, surname, email, password, age, country, role)
 VALUES ('$name', '$surname', '$email', '$pass', '$age', '$country', '$role')";
+**/
 
+$sql = "INSERT INTO users (name, surname, email, password, age, country, role)
+VALUES (':name', ':surname', ':email', ':pass', ':age', ':country', ':role')";
+
+$stmt = $db->prepare($sql);
+
+$stmt->execute(array(
+    ":name" => $name,
+    ":surname" => $surname,
+    ":email" => $email,
+    ":pass" => $pass,
+    ":age" => $age,
+    ":country" => $country,
+    ":role" => $role
+));
+
+/*
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
-
+**/
 
 $conn->close();
 header("Location:login.php");
