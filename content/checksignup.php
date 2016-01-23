1d2d0e4520e5f3bbf692e8976746e6ee37660fe1
Thanasis <tsiarasa@hotmail.com>
2016-01-23 15:59:27 +0000

    Update checksignup.php

--- content/checksignup.php
+++ content/checksignup.php
@@ -1,11 +1,25 @@
 <?php
 
+$name=$_POST['name'];
+$surname=$_POST['surname'];
+$email=$_POST['email'];
+$pass=$_POST['password'];
+$age=$_POST['age'];
+$country=$_POST['country'];
+$role=$_POST['role'];
+
 $hostname="172.17.0.2:3306";
 $database="netflixcloneproject";
 $username="root";
 $password="999999";
-$link=mysql_connect($hostname, $username, $password) or
-	die ("An error occured while connecting to the server!" .mysql_error());
-mysql_select_db($database,$link);
 
+$conn = new mysqli($hostname, $username, $password, $database);
+
+$sql = "INSERT INTO users (name, surname, email, password, age, country, role)
+VALUES ('$name', '$surname', '$email', '$pass', '$age', '$country', '$role')";
+
+$conn->query($sql);
+
+$conn->close();
+header("Location:login.php");
 ?>
