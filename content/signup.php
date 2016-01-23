aa0ec98341bb652f392267dfcff55f1fe774fda3
Thanasis <tsiarasa@hotmail.com>
2016-01-23 11:47:57 +0000

    Update signup.php

--- content/signup.php
+++ content/signup.php
@@ -1,3 +1,14 @@
 <?php
-echo 'cool';
+
+$hostname="172.17.0.2:3306";
+$database="netflixcloneproject";
+$username="root";
+$password="999999";
+
+$link=mysql_connect($hostname, $username, $password) or
+	die ("An error occured while connecting to the server!" .mysql_error());
+mysql_select_db($database,$link);
+
+
+
 ?>
