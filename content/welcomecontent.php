8b0a3a224fec0f68aa4dacf8a25e7a2b9de04f0f
Thanasis <tsiarasa@hotmail.com>
2016-01-23 20:39:11 +0000

    Update welcomecontent.php

--- content/welcomecontent.php
+++ content/welcomecontent.php
@@ -5,8 +5,8 @@ if(!isset($_SESSION['role']))
 	header("location:login.php");
 	}
 echo '1';
-require_once("jpgraph/jpgraph.php");
-require_once("jpgraph/jpgraph_bar.php");
+require_once("/jpgraph/jpgraph.php");
+require_once("/jpgraph/jpgraph_bar.php");
 echo '2';
 $connection = new Mongo("mongodb://172.17.0.3");
 $db = $connection->CATALOGUE;
