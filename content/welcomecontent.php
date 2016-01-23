9f582705b5ae893b7f994f4e7b25552fe500a859
Thanasis <tsiarasa@hotmail.com>
2016-01-23 20:18:29 +0000

    Update welcomecontent.php

--- content/welcomecontent.php
+++ content/welcomecontent.php
@@ -9,8 +9,14 @@ $connection = new Mongo("mongodb://172.17.0.3");
 $db = $connection->CATALOGUE;
 $collection = $db->movies;
 $cursor = $collection->find();
+
+$data=array();
+
 foreach ($cursor as $document) {
+	array_push($data, $document['likes']);
 }
+
+print_r($data);
 
 
 
