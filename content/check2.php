65a89b8985fa926b44eb86b2daed7f4d8c684da7
Thanasis <tsiarasa@hotmail.com>
2016-01-23 17:31:09 +0000

    Create check2.php

---
+++ content/check2.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+function clean($str) {
+		$str = @trim($str);
+		if(get_magic_quotes_gpc()) {
+			$str = stripslashes($str);
+		}
+		return mysql_real_escape_string($str);
+	}
+$hostname="172.17.0.2:3306";
+$database="netflixcloneproject";
+$username="root";
+$password="999999";
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+$link=mysql_connect($hostname, $username, $password) or
+	die ("An error occured while connecting to the server!" .mysql_error());
+mysql_select_db($database,$link);
+$Qcheck_user="SELECT * FROM users WHERE email='$user' AND password='$pass'";
+$check_user=mysql_query($Qcheck_user, $link)or
+	die("There was an error:".mysql_error());
+$number=mysql_num_rows($check_user);
+$query="SELECT * FROM users";
+$results = mysql_query($query);
+if($number==0)
+{
+	header("location:login.php");
+}
+else
+{
+	$user=mysql_fetch_array($check_user);
+	
+	$role=$user["role"];
+	
+	$_SESSION['role'] = $user['role'];
+	$_SESSION['email'] = $user['email'];
+	$_SESSION['name'] = $user['name'];
+	$_SESSION['surname'] = $user['surname'];
+	
+	header("Location:index.php");
+}
+?>
