672e9bcd578abe429245cea7f038366e27611240
Thanasis <tsiarasa@hotmail.com>
2016-01-23 12:03:20 +0000

    Update signup.php

--- content/signup.php
+++ content/signup.php
@@ -1,14 +1,50 @@
-<?php
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 
-$hostname="172.17.0.2:3306";
-$database="netflixcloneproject";
-$username="root";
-$password="999999";
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>Sign up</title>
+<link href="styles/style1.css" rel="stylesheet" type="text/css" />
 
-$link=mysql_connect($hostname, $username, $password) or
-	die ("An error occured while connecting to the server!" .mysql_error());
-mysql_select_db($database,$link);
+</head>
 
+<body>
 
+<div id="container">
+<div id="content">
+<div id="center">
 
-?>
+<h2>Sign up for NetflixCloneProject</h2><br></br>
+<form action="checksignup.php" method="post">
+	Name:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" name="name"/>
+	<br></br>
+	
+	Surname:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" name="surname"/>
+	<br></br>
+	
+	E-mail:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" name="email"/>
+	<br></br>
+	
+	Age:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" name="age"/>
+	<br></br>
+	
+	Country:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" name="country"/>
+	<br></br>
+	
+	Subscription type:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" name="role"/>
+	<br></br>
+	
+	Password:&nbsp; <input type="password" name="pass"/>
+	<br></br>
+	
+	<input type="submit" name="login" value="Login"/>
+	<br></br>
+	
+</form>
+<input type="submit" name="signup" value="Sign up"  onclick="location.href='signup.php';"/>
+</div>
+</div>
+</div>
+</body>
+
+</html>
