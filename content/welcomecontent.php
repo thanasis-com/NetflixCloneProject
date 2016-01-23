4825f9897cfbcd542743f947743b3469c8a71d92
Thanasis <tsiarasa@hotmail.com>
2016-01-23 20:13:14 +0000

    Create welcomecontent.php

---
+++ content/welcomecontent.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+if(!isset($_SESSION['role']))
+{
+	header("location:login.php");
+	}
+
+$connection = new Mongo("mongodb://172.17.0.3");
+$db = $connection->CATALOGUE;
+$collection = $db->movies;
+$cursor = $collection->find();
+foreach ($cursor as $document) {
+}
+
+
+
+	
+?>
