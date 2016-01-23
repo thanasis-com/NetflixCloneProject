8913f82d93e8f10ee718318df1e9d869457aa449
Thanasis <tsiarasa@hotmail.com>
2016-01-23 17:32:08 +0000

    Update check2.php

--- content/check2.php
+++ content/check2.php
@@ -7,21 +7,30 @@ function clean($str) {
 		}
 		return mysql_real_escape_string($str);
 	}
+	
 $hostname="172.17.0.2:3306";
 $database="netflixcloneproject";
 $username="root";
 $password="999999";
 $user = $_POST['user'];
 $pass = $_POST['pass'];
+
 $link=mysql_connect($hostname, $username, $password) or
 	die ("An error occured while connecting to the server!" .mysql_error());
+	
 mysql_select_db($database,$link);
+
 $Qcheck_user="SELECT * FROM users WHERE email='$user' AND password='$pass'";
+
 $check_user=mysql_query($Qcheck_user, $link)or
 	die("There was an error:".mysql_error());
+	
 $number=mysql_num_rows($check_user);
+
 $query="SELECT * FROM users";
+
 $results = mysql_query($query);
+
 if($number==0)
 {
 	header("location:login.php");
