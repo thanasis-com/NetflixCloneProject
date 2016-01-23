50242ba89516a46109e8ebb904ab7d0eb76f5317
Thanasis <tsiarasa@hotmail.com>
2016-01-23 17:10:37 +0000

    Update checksignup2.php

--- content/checksignup2.php
+++ content/checksignup2.php
@@ -12,13 +12,26 @@ $database="netflixcloneproject";
 $username="root";
 $password="999999";
 
+// A higher "cost" is more secure but consumes more processing power
+$cost = 10;
+
+// Create a random salt
+$salt = strtr(base64_encode(mcrypt_create_iv(16, MCRYPT_DEV_URANDOM)), '+', '.');
+
+// Prefix information about the hash so PHP knows how to verify it later.
+// "$2a$" Means we're using the Blowfish algorithm. The following two digits are the cost parameter.
+$salt = sprintf("$2a$%02d$", $cost) . $salt;
+
+// Hash the password with the salt
+$hash = crypt($password, $salt);
+
 $conn = new mysqli($hostname, $username, $password, $database, 3306);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
 $sql = "INSERT INTO users (name, surname, email, password, age, country, role)
-VALUES ('$name', '$surname', '$email', '$pass', '$age', '$country', '$role')";
+VALUES ('$name', '$surname', '$email', '$hash', '$age', '$country', '$role')";
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
