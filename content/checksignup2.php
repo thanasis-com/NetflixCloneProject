6644789b6ce4b1dcc7ce6f51d7a5dc4cd5f270e9
Thanasis <tsiarasa@hotmail.com>
2016-01-23 17:07:14 +0000

    Create checksignup2.php

---
+++ content/checksignup2.php
@@ -0,0 +1,26 @@
+<?php
+$name=$_POST['name'];
+$surname=$_POST['surname'];
+$email=$_POST['email'];
+$pass=$_POST['password'];
+$age=$_POST['age'];
+$country=$_POST['country'];
+$role=$_POST['role'];
+$hostname="172.17.0.2";
+$database="netflixcloneproject";
+$username="root";
+$password="999999";
+$conn = new mysqli($hostname, $username, $password, $database, 3306);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$sql = "INSERT INTO users (name, surname, email, password, age, country, role)
+VALUES ('$name', '$surname', '$email', '$pass', '$age', '$country', '$role')";
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+header("Location:login.php");
+?>
