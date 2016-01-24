49143832c0f29d8f41e6c4751efc36b43fd3c372
Thanasis <tsiarasa@hotmail.com>
2016-01-24 10:47:05 +0000

    Update piechart.php

--- content/piechart.php
+++ content/piechart.php
@@ -8,6 +8,21 @@ if(!isset($_SESSION['role']))
 require_once ('/home/src/jpgraph.php');
 require_once ('/home/src/jpgraph_pie.php');
 require_once ('/home/src/jpgraph_pie3d.php');
+
+$likes=array();
+$labels=array();
+foreach ($cursor as $document) {
+
+	if(in_array($document['genre'],$labels)
+	{
+		$likes[array_search($document['genre'], $labels)]=$likes[array_search($document['genre'], $labels)]+$document['likes'];
+	}
+	else
+	{
+		array_push($labels,$document['genre']);
+		array_push($likes, $document['likes']);
+	}
+}
 	
 	
 	
