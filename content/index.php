73be17de7e42dccc2675f6a8e12268f40b7f587e
Thanasis <tsiarasa@hotmail.com>
2016-01-24 10:26:16 +0000

    Update index.php

--- content/index.php
+++ content/index.php
@@ -69,8 +69,8 @@ function logout(){
 	<div id="content">
 		<!-- #BeginEditable "content" -->
 		<div id="videos">
-		<p class="welcome">Welcome to NetflixCloneProject!</p>
-		<p class="welcome">Here is what other users liked: </p>
+		<p class="welcome1">Welcome to NetflixCloneProject!</p>
+		<p class="welcome2">Here is what other users liked: </p>
 		<img src="barchart.php">
 		</div>
 		
