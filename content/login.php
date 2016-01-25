7890ba14b2a94dcc22cdd1c27750f0f1f0b8fe70
Thanasis <tsiarasa@hotmail.com>
2016-01-25 19:30:12 +0000

    Update login.php

--- content/login.php
+++ content/login.php
@@ -6,7 +6,14 @@
 <title>Login</title>
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
 <link href="styles/style1.css" rel="stylesheet" type="text/css" />
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 
 <body>
