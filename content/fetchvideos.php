6c58d556de36fe394be950c18b7c93e19f48e837
Thanasis <tsiarasa@hotmail.com>
2016-01-25 12:21:44 +0000

    Update fetchvideos.php

--- content/fetchvideos.php
+++ content/fetchvideos.php
@@ -11,7 +11,7 @@ $cursor = $collection->find();
 foreach ($cursor as $document) {
     if($document["visibility"]==$_SESSION['role'] || $_SESSION['role']=="prime" || $_SESSION['role']=="admin"){
 		
-      echo '<p class="title">'.$document["title"].'</p>'.'<video class="video-js vjs-default-skin" width="640px" height="267px" controls preload="auto" poster="img/bbb.png" data-setup="{}"> <source src="rtmp://netflixcloneproject.cloudapp.net:1935/vod2/'.$document["filename"].'" type="rtmp/mp4" /></video>'.'<p><div class="label">Description: </div>'.$document["description"].'</p>'.'<p><div class="label">Genre: </div>'.$document["genre"].'</p>'.'<p><div class="label">Likes: </div>'.$document["likes"].'</p>'.'<p><input type="image" id="likebutton" name="likebutton" value="'.$document["_id"].'" src="/img/likebutton.png" onclick="like(this.value);sendmail(this.value);" /><hr>';                           
+      echo '<p class="title">'.$document["title"].'</p>'.'<video class="video-js vjs-default-skin" width="640px" height="267px" controls preload="auto" poster="img/'.$document["title"].'.png" data-setup="{}"> <source src="rtmp://netflixcloneproject.cloudapp.net:1935/vod2/'.$document["filename"].'" type="rtmp/mp4" /></video>'.'<p><div class="label">Description: </div>'.$document["description"].'</p>'.'<p><div class="label">Genre: </div>'.$document["genre"].'</p>'.'<p><div class="label">Likes: </div>'.$document["likes"].'</p>'.'<p><input type="image" id="likebutton" name="likebutton" value="'.$document["_id"].'" src="/img/likebutton.png" onclick="like(this.value);sendmail(this.value);" /><hr>';                           
     }
    }
 ?>
