e70cbb1239bae49440359c75f12b027035bb964c
Thanasis <tsiarasa@hotmail.com>
2016-01-25 19:09:09 +0000

    Update all.php

--- content/all.php
+++ content/all.php
@@ -21,7 +21,7 @@ if(!isset($_SESSION['role']))
 <link href="http://vjs.zencdn.net/4.11/video-js.css" rel="stylesheet">
 <script src="http://vjs.zencdn.net/4.11/video.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+
 
 <script type="text/javascript">
 
