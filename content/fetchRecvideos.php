6e3e6a44ed2ddb038d98ac314b8b9159777f68d2
Thanasis <tsiarasa@hotmail.com>
2016-01-25 21:55:54 +0000

    Update fetchRecvideos.php

--- content/fetchRecvideos.php
+++ content/fetchRecvideos.php
@@ -4,7 +4,7 @@ if(!isset($_SESSION['role']))
 {
 	header("location:login.php");
 	}
-
+echo '1';
 $connection = new Mongo("mongodb://172.17.0.3");
 $db = $connection->CATALOGUE;
 $collection = $db->movies;
