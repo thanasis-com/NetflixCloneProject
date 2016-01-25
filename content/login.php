c2524c1bd4ce099674eff42858eaef75b0cb783f
Thanasis <tsiarasa@hotmail.com>
2016-01-25 19:26:36 +0000

    Update login.php

--- content/login.php
+++ content/login.php
@@ -2,12 +2,13 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+
 <title>Login</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
 <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+<link href="styles/style1.css" rel="stylesheet" type="text/css" />
 
 </head>
 
