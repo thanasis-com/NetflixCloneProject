cc5cabfa0b07294de7cf42fe5ccd44888fdbb7bd
Thanasis <tsiarasa@hotmail.com>
2016-01-25 18:33:37 +0000

    Update fetchcomments.php

--- content/fetchcomments.php
+++ content/fetchcomments.php
@@ -1,7 +1,12 @@
 <?php
-function first($id){
-	echo $id;
-}
+function fetchcomments($id){
+	$connection = new Mongo("mongodb://172.17.0.3");
+	$db = $connection->CATALOGUE;
+	$collection = $db->movies;
+	$cursor = $collection->find();
+	foreach ($cursor as $document) {
+		echo <li><a href="#">Calvin</a></li>;
+	}
 
 
 ?>
