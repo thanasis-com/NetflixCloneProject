a8f1e68aada52451c47fe0c1d00a9b8427a3aa9e
Thanasis <tsiarasa@hotmail.com>
2016-01-25 22:04:11 +0000

    Update fetchRecvideos.php

--- content/fetchRecvideos.php
+++ content/fetchRecvideos.php
@@ -9,5 +9,41 @@ $connection = new Mongo("mongodb://172.17.0.3");
 $db = $connection->CATALOGUE;
 $collection = $db->movies;
 
+echo '2';
+if($_SESSION['animated']==1)
+{
+  $cursor = $collection->find(array('genre' => "animated"));
+  $cursor->sort(array('likes' => -1))->limit(1);
+  foreach ($cursor as $document) {
+    if($document["visibility"]==$_SESSION['role'] || $_SESSION['role']=="prime" || $_SESSION['role']=="admin"){
+		
+      echo '<p class="title">'.$document["title"].'</p>'.'<video class="video-js vjs-default-skin" width="640px" height="267px" controls preload="auto" poster="img/'.$document["title"].'.png" data-setup="{}"> <source src="rtmp://netflixcloneproject.cloudapp.net:1935/vod2/'.$document["filename"].'" type="rtmp/mp4" /></video>'.'<p><div class="label">Description: </div>'.$document["description"].'</p>'.'<p><div class="label">Genre: </div>'.$document["genre"].'</p>'.'<p><div class="label">Likes: </div>'.$document["likes"].'</p>'.'<p><input type="image" id="likebutton" name="likebutton" value="'.$document["_id"].'" src="/img/likebutton.png" onclick="like(this.value);sendmail(this.value);" /><hr>';
+    
+    }
+   }
+}
+if($_SESSION['music']==1)
+{
+  $cursor = $collection->find({"genre":"music"}).sort({"likes": -1}).limit(1);
+  foreach ($cursor as $document) {
+    if($document["visibility"]==$_SESSION['role'] || $_SESSION['role']=="prime" || $_SESSION['role']=="admin"){
+		
+      echo '<p class="title">'.$document["title"].'</p>'.'<video class="video-js vjs-default-skin" width="640px" height="267px" controls preload="auto" poster="img/'.$document["title"].'.png" data-setup="{}"> <source src="rtmp://netflixcloneproject.cloudapp.net:1935/vod2/'.$document["filename"].'" type="rtmp/mp4" /></video>'.'<p><div class="label">Description: </div>'.$document["description"].'</p>'.'<p><div class="label">Genre: </div>'.$document["genre"].'</p>'.'<p><div class="label">Likes: </div>'.$document["likes"].'</p>'.'<p><input type="image" id="likebutton" name="likebutton" value="'.$document["_id"].'" src="/img/likebutton.png" onclick="like(this.value);sendmail(this.value);" /><hr>';
+    
+    }
+   }
+}
+if($_SESSION['documentary']==1)
+{
+  $cursor = $collection->find({"genre":"documentary"}).sort({"likes": -1}).limit(1);
+  foreach ($cursor as $document) {
+    if($document["visibility"]==$_SESSION['role'] || $_SESSION['role']=="prime" || $_SESSION['role']=="admin"){
+		
+      echo '<p class="title">'.$document["title"].'</p>'.'<video class="video-js vjs-default-skin" width="640px" height="267px" controls preload="auto" poster="img/'.$document["title"].'.png" data-setup="{}"> <source src="rtmp://netflixcloneproject.cloudapp.net:1935/vod2/'.$document["filename"].'" type="rtmp/mp4" /></video>'.'<p><div class="label">Description: </div>'.$document["description"].'</p>'.'<p><div class="label">Genre: </div>'.$document["genre"].'</p>'.'<p><div class="label">Likes: </div>'.$document["likes"].'</p>'.'<p><input type="image" id="likebutton" name="likebutton" value="'.$document["_id"].'" src="/img/likebutton.png" onclick="like(this.value);sendmail(this.value);" /><hr>';
+    
+    }
+   }
+}
+
 echo '4';
 ?>
