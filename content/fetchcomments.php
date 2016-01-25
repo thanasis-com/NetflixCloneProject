54bd0ed0124b22a9f701f351c60856fc52f482cd
Thanasis <tsiarasa@hotmail.com>
2016-01-25 18:10:37 +0000

    Update fetchcomments.php

--- content/fetchcomments.php
+++ content/fetchcomments.php
@@ -6,7 +6,7 @@ if(!isset($_SESSION['role']))
 	}
 	
 try{
-$connection = new Mongo("mongodb://172.17.0.7");
+$connection = new Mongo("mongodb://172.17.0.7:27017");
 $db = $connection->COMMENTS;
 $collection = $db->comments;
 $cursor = $collection->find();
