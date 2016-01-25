d4652d02f5d69b054eed8e64e333f3c49bab7ce1
Thanasis <tsiarasa@hotmail.com>
2016-01-25 16:19:16 +0000

    Update documentaries.php

--- content/documentaries.php
+++ content/documentaries.php
@@ -61,6 +61,8 @@ function logout(){
 			<li><a href="music.php">Music videos</a></li>
 		</ul>
 	<hr>
+	<a href="userspreferences.php">Users' preferences</a>
+	<hr>
 	<?php include 'logininfo.php'; ?>
 	</div>
 	<!-- End Navigation -->
