1401597e08b2162689b3d9a529939126bace5de7
Thanasis <tsiarasa@hotmail.com>
2016-01-25 18:58:48 +0000

    Update all.php

--- content/all.php
+++ content/all.php
@@ -24,7 +24,6 @@ if(!isset($_SESSION['role']))
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
 
-<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 <script type="text/javascript">
 
 function like(value) {
