26a3b09edfea22a36e61422f6a3ed8ccc23c7d66
Thanasis <tsiarasa@hotmail.com>
2016-01-25 18:57:42 +0000

    Update all.php

--- content/all.php
+++ content/all.php
@@ -23,7 +23,7 @@ if(!isset($_SESSION['role']))
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
-<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+
 <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 <script type="text/javascript">
 
