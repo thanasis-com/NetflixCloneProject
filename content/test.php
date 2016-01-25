4d1bd87bcc8afe26402038f8cc60685b5379d654
Thanasis <tsiarasa@hotmail.com>
2016-01-25 16:00:27 +0000

    Create test.php

---
+++ content/test.php
@@ -0,0 +1,13 @@
+<?php
+
+$hostname="172.17.0.6:3306";
+$database="netflixcloneproject";
+$username="root";
+$password="999999";
+
+$link=mysql_connect($hostname, $username, $password) or
+	die ("An error occured while connecting to the server!" .mysql_error());
+	
+echo $link;
+	
+	?>
