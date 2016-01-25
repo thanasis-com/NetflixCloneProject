9fa1f61bf9461f671244acee03ef0da6f9a59af9
Thanasis <tsiarasa@hotmail.com>
2016-01-25 19:25:04 +0000

    Update login.php

--- content/login.php
+++ content/login.php
@@ -7,7 +7,7 @@
 <link href="styles/style1.css" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
 <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
 
 </head>
 
